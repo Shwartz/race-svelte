09c30db6bd4600407e6520a2483b06c2a9fe449d
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-10-01 18:02:20 +0100

    Improved base config for dev and prod env

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+console.log({dev})
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -14,7 +17,7 @@ const config = {
 			fallback: 'index.html',
 		}),
 		paths: {
-			base: '/race-svelte',
+			base: dev ?  '' : '/race-svelte',
 		}
 	}
 };
