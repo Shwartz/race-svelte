c6bc9f81126fd94e54f97377d1057b05a8c99700
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-10-02 10:07:45 +0100

    Adding site to docs and nojekyll to escape Jekyll SSG

--- docs/_app/immutable/chunks/0-d58add88.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-53b7f33e.js";export{t as component};
--- docs/_app/immutable/chunks/5-f1dbca03.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/todos/_page.svelte-0b1ad0f7.js";const e=!0;export{t as component,e as server};
